758e56b5f63eb6336a130c1bc3ce85a301eac45f
kawasima <kawasima1016@gmail.com>
2014-01-06 16:18:22 +0900

    init

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject net.unit8/mine-canary "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/kawasima/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :aot :all
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [storm "0.9.0.1"]])
---
+++ test/mine_canary/core_test.clj
@@ -0,0 +1,7 @@
+(ns mine-canary.core-test
+  (:require [clojure.test :refer :all]
+            [mine-canary.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
